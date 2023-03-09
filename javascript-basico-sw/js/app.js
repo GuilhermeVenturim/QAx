0d580211d7468ec5de491be296257f64b46ad596
Guilherme Venturim <guilhermeventurim@outlook.com>
2023-03-09 18:12:52 -0300

    Add botão like

--- javascript-basico-sw/js/app.js
+++ javascript-basico-sw/js/app.js
@@ -1,4 +1,6 @@
 
+
+//Lista de itens
 const LIST = [
 
     {
@@ -6,59 +8,42 @@ const LIST = [
         nome: 'Mestre Yoda',
         avatar: 'img/yoda.png'
     },
-    
     {
         id: 2,
         nome: 'Luke Skywalker',
         avatar: 'img/luke.png'
     },
-    
     {
         id: 3,
         nome: 'Princesa Leia',
         avatar: 'img/leia.png'
     },
-    
     {
         id: 4,
         nome: 'Han Solo',
         avatar: 'img/hansolo.png'
     },
-    
     {
         id: 5,
         nome: 'Darth Vader',
         avatar: 'img/vader.png'
     },
-    
     {
         id: 6,
         nome: 'Chewbacca',
         avatar: 'img/chewbacca.png'
     },
-    
     {
         id: 7,
         nome: 'R2D2',
         avatar: 'img/r2d2.png'
     },
-    
     {
         id: 8,
         nome: 'C3pO',
         avatar: 'img/c3po.png'
     }
 ]
-
-
-
-
-
-
-
-
-
-
 
 
 //Criando uma constante com nome APP, informando uma nova instância do VueJS
@@ -68,6 +53,11 @@ const App = new Vue({
         title: 'Star Wars Lego',
         userName: 'Guilherme Venturim',
         characters: LIST
+    },
+    methods: {
+        like(userName) {
+            alert(`O personagem ${userName} recebeu um like!`)
+        }
     }
 })
 
