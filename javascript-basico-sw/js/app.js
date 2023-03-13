0f4de2b9e31cbc227630cd366b6a12c85ef02610
Guilherme Venturim <guilhermeventurim@outlook.com>
2023-03-13 11:46:11 -0300

    Add sistema de busca por personagem. ADD botão remover. Usado filter, if, else como controle de fluxo

--- javascript-basico-sw/js/app.js
+++ javascript-basico-sw/js/app.js
@@ -52,11 +52,41 @@ const App = new Vue({
     data: {
         title: 'Star Wars Lego',
         userName: 'Guilherme Venturim',
-        characters: LIST
+        characters: LIST,
+        searchName: ''                                  //Valor impresso no campo de busca
     },
     methods: {
         like(userName) {
             alert(`O personagem ${userName} recebeu um like!`)
+        },
+        remove(id){
+            const list = this.characters
+
+            const result = list.filter(item => {
+                return item.id !== id
+            })
+
+            this.characters = result
+        },
+        search() {
+
+            if (this.searchName === '') {
+                return alert('Digite o que deseja pesquisar no campo de busca.')
+            }                                           //return é usado para não executar o bloco abaixo caso a condição seja aceita.
+                                                        //"LIST" no fim da const "list" é usado para retornar a lista original (LIST). Sem isso, sempre que uma busca for realizada, a const "list" terá somente o item buscado, não podendo buscar outros itens da "LIST"
+            const list = this.characters = LIST         //Carrega todos os itens da lista na const list.
+                                                        //"this" refere-se a váriável (characters)
+
+            const result = list.filter(item => {        //argumento "item" se refere a um item na lista
+                return item.nome === this.searchName    //Uso de arrow function em Vuejs
+            })
+
+            if (result.length <= 0) {                   //Controle de fluxo
+                alert('Nenhum registro encontrado.')
+            } else {
+                this.characters = result
+            }
+
         }
     }
 })
